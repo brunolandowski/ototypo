bf8572c7b59986d0298aafe26860e7a4fc0351bd
Bruno Landowski <bruno.landowski@gmail.com>
2016-10-17 10:16:45 +0200

    Update ototypo.js

--- ototypo.js
+++ ototypo.js
@@ -2,18 +2,18 @@
     $.fn.ototypo = function(options) {
 
         var defauts = {
-        	'QuestionMark': true, 		        // points d'interrogation   	-> !
-            'ExclamationPoint': true, 	        // points d'exclamation 	    -> ?
+        	QuestionMark: true, 		        // points d'interrogation   	-> !
+            ExclamationPoint: true, 	        // points d'exclamation 	    -> ?
             
-        	'Colon': true, 				        // deux-points 			        -> :
-        	'SemiColon': true, 			        // point-virgules 		        -> ;
+        	Colon: true, 				        // deux-points 			        -> :
+        	SemiColon: true, 			        // point-virgules 		        -> ;
 
-        	'Parenthesis': true, 		        // parenthèses			        -> ()
+        	Parenthesis: true, 		        // parenthèses			        -> ()
 
-        	'CurlyBrace': true, 		        // accolades			        -> {}
-            'Percentage': true,                 // pourcentage                  -> %
-            'QuotationMarks': true,             // guillemets                   -> «»
-            'Ellipsis': true                    // points de suspension         -> …
+        	CurlyBrace: true, 		        // accolades			        -> {}
+            Percentage: true,                 // pourcentage                  -> %
+            QuotationMarks: true,             // guillemets                   -> «»
+            Ellipsis: true                    // points de suspension         -> …
            
         };
 
